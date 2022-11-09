c06cd4bca9a47e26c29f92a7f6f7306e9dc4ac82
ilify <97926378+ilify@users.noreply.github.com>
2022-11-09 22:06:49 +0100

    Update Bullet.cs

--- Player/Bullet.cs
+++ Player/Bullet.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool hit = false;
     void Start()
     {
         Destroy(gameObject, 5f);
     }
-
+    private void Update()
+    {
+        if(GetComponent<Rigidbody>().velocity != Vector3.zero) GetComponent<Rigidbody>().velocity += transform.up * -9.8f * Time.deltaTime;
+    }
     // Update is called once per frame
     private void OnCollisionEnter(Collision collision)
     {
